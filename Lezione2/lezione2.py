16811765345f130cdcc00a85a774f3798571de91
Lorenzo-Trombus <lorenzotrombini28@gmail.com>
2024-04-18 14:00:02 +0200

    fatto esercizio 8

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -48,7 +48,31 @@ print(filename.removesuffix(".txt"))
 
 
 names: list=["osama","furetto","danila","manuel"]
-for i in names[0:5]:
+for i in names:
     print(f"{i} te voglio bene")
-    
 
+
+#Your Own List: Think of your favorite mode of transportation, such as a motorcycle or a car,
+# and make a list that stores several examples. 
+#Use your list to print a series of statements about these items, such as “I would like to own a Honda motorcycle.”
+
+cars: list=["mercedes","bmw","alfa romeo", "lamborghini"]
+for i in cars:
+    print(f"mi piacerebbe avere una {i}")
+
+#Guest List: If you could invite anyone, living or deceased, to dinner, who would you invite?
+# Make a list that includes at least three people you’d like to invite to dinner.
+# Then use your list to print a message to each person, inviting them to dinner.
+"""
+3-5. Changing Guest List: You just heard that one of your guests can’t make the dinner, so you need to send out a new set of invitations. You’ll have to think of someone else to invite.
+• Start with your program from Exercise 3-4. Add a print() call at the end of your program, stating the name of the guest who can’t make it.
+• Modify your list, replacing the name of the guest who can’t make it with the name of the new person you are inviting.
+• Print a second set of invitation messages, one for each person who is still in your list.
+
+3-6. More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+• Start with your program from Exercise 3-4 or 3-5. Add a print() call to the end of your program, informing people that you found a bigger table.
+• Use insert() to add one new guest to the beginning of your list.
+• Use insert() to add one new guest to the middle of your list.
+• Use append() to add one new guest to the end of your list.
+• Print a new set of invitation messages, one for each person in your list.
+""" 
