429943132c53e1ae45677a6c7044784640dd8bc4
Lorenzo-Trombus <lorenzotrombini28@gmail.com>
2024-04-18 12:50:49 +0200

    risolto secondo esercizio

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -5,6 +5,18 @@ print("hello world")
 
 #Personal Message: Use a variable to represent a person’s name, and print a message to that person.
 # Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-x= str(input("scrivi il tuo nome:"))
+#x= str(input("scrivi il tuo nome:"))
+x:str="pippo"
 mess: str= f"ciao {x},ti va di imparare python?"
 print(mess)  
+
+"""
+ Name Cases: Use a variable to represent a person’s name,
+ and then print that person’s name in lowercase, uppercase, and title case.
+"""
+
+#y= str(input("scrivi il tuo nome:"))
+y:str="baudo pippo"
+print(f"{y.upper()},{y.lower()}, {y.title()}")
+ 
+print("")
