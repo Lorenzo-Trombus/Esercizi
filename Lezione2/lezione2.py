4269e5bea5660f2aeb6ae364d71a571fa922d111
Lorenzo-Trombus <lorenzotrombini28@gmail.com>
2024-04-23 19:38:39 +0200

    fatto esercizio 3.8 lezioene 2

--- Lezione2/lezione2.py
+++ Lezione2/lezione2.py
@@ -81,20 +81,90 @@ Think of three more guests to invite to dinner.
 • Use append() to add one new guest to the end of your list.
 • Print a new set of invitation messages, one for each person in your list.
 """   
+
+"""
+3-7. Shrinking Guest List: You just found out that your new dinner table won’t arrive in time for the dinner, and now you have space for only two guests.
+• Start with your program from Exercise 3-6. Add a new line that prints a message saying that you can invite only two people for dinner.
+• Use pop() to remove guests from your list one at a time until only two names remain in your list.
+ Each time you pop a name from your list, print a message to that person letting them know you’re sorry you can’t invite them 
+ to dinner.
+• Print a message to each of the two people still on your list, letting them know they’re still invited.
+• Use del to remove the last two names from your list, so you have an empty list. Print your list to make sure you actually have an empty list at the end of your program.
+"""
+
 invitati=["Ian Anderson","Mohamed Alì","Jimi Hendrix"]
 invitati.insert(0,"Bob Marley")
 invitati.insert(2,"Jimmy Page")
 invitati.append("Jim Morrison")
-for i in invitati:
-    print(f"ciao {i}, ti va di venire a cena con me?")
+for i in range(len(invitati)-2):
+    invitati.pop(0)
+    print(f"sorry {invitati[0]}, you are not invited to dinner anyore")
+
+for i in invitati:    
+    print(f"ciao {i}, ti va di venire a cena da me?")
     
 print("Totti nun po' venì")
 print("ho trovato un tavolo più grande")
+print("there will be only two seats at the table")
 
 
 
 
+"""
+3-8. Seeing the World: Think of at least five places in the world you’d like to visit.
+• Store the locations in a list. Make sure the list is not in alphabetical order.
+• Print your list in its original order. Don’t worry about printing the list neatly; just print it as a raw Python list.
+• Use sorted() to print your list in alphabetical order without modifying the actual list.
+• Show that your list is still in its original order by printing it.
+• Use sorted() to print your list in reverse-alphabetical order without changing the order of the original list.
+• Show that your list is still in its original order by printing it again.
+• Use reverse()  to change the order of your list. Print the list to show that its order has changed.
+• Use reverse() to change the order of your list again. Print the list to show it’s back to its original order.
+• Use sort() to change your list so it’s stored in alphabetical order. Print the list to show that its order has been changed.
+• Use sort() to change your list so it’s stored in reverse-alphabetical order.
+Print the list to show that its order has changed.
+"""
+places_to_visit=["maldive","hawaii","amsterdam","bruges","hollywood"]
+print(places_to_visit)
+print(sorted(places_to_visit))
+print(places_to_visit)
+print(sorted(places_to_visit, reverse=True))
+print(places_to_visit)
+places_to_visit.reverse()
+print(places_to_visit)
 
+"""
+3-9. Dinner Guests: Working with one of the programs from Exercises 3, use len() to print a message indicating the number of people you’re inviting to dinner.
+"""
+#3-10. Every Function: Think of things you could store in a list. For example, you could make a list of mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program that creates a list containing these items and then uses each function introduced in this chapter at least once.
+
+
+"""
+6-1. Person: Use a dictionary to store information about a person you know. Store their first name, last name, age, and the city in which they live. You should have keys such as first_name, last_name, age, and city. Print each piece of information stored in your dictionary.
+"""
+"""
+6-2. Favorite Numbers: Use a dictionary to store people’s favorite numbers. Think of five names, and use them as keys in your dictionary. Think of a favorite number for each person, and store each as a value in your dictionary. Print each person’s name and their favorite number. For even more fun, poll a few friends and get some actual data for your program.
+"""
+"""
+6-3. Glossary: A Python dictionary can be used to model an actual dictionary. However, to avoid confusion, let’s call it a glossary.
+• Think of five programming words you’ve learned about in the previous chapters. Use these words as the keys in your glossary, and store their meanings as values.
+• Print each word and its meaning as neatly formatted output. You might print the word followed by a colon and then its meaning, or print the word on one line and then print its meaning indented on a second line. Use the newline character (\n) to insert a blank line between each word-meaning pair in your output.
+"""
+"""
+6-7. People: Start with the program you wrote for Exercise 6-1. Make two new dictionaries representing different people, and store all three dictionaries in a list called people. Loop through your list of people. As you loop through the list, print everything you know about each person.
+"""
+"""
+6-8. Pets: Make several dictionaries, where each dictionary represents a different pet. In each dictionary, include the kind of animal and the owner’s name. Store these dictionaries in a list called pets. Next, loop through your list and as
+you do, print everything you know about each pet. 
+"""
+"""
+6-9. Favorite Places: Make a dictionary called favorite_places. Think of three names to use as keys in the dictionary, and store one to three favorite places for each person. To make this exercise a bit more interesting, ask some friends to name a few of their favorite places. Loop through the dictionary, and print each person’s name and their favorite places.
+6-10. Favorite Numbers: Modify your program from Exercise 6-2 so each person can have more than one favorite number. Then print each person’s name along with their favorite numbers.
+6-11. Cities: Make a dictionary called cities. Use the names of three cities as keys in your dictionary. Create a dictionary of information about each city and include the country that the city is in, its approximate population, and one fact about that city. The keys for each city’s dictionary should be something like country, population, and fact. Print the name of each city and all of the information you have stored about it.
+"""
+"""
+6-12. Extensions: We’re now working with examples that are complex enough that they can be extended in any number of ways. Use one of the example programs from this chapter, and extend it by adding new keys and values, changing the context of the program, or improving the formatting of the output.
+"""
 #esempio fatto in classe su max e min di lista di stringhe
 """
 listastr: str= ["assagia","le", "mele","piccole"]
