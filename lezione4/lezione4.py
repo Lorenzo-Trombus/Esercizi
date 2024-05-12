a2883fdd17711b9e280acb8f77897fa7badfea67
Lorenzo-Trombus <lorenzotrombini28@gmail.com>
2024-05-12 18:59:08 +0200

    es 8-14 lezione 4

--- lezione4/lezione4.py
+++ lezione4/lezione4.py
@@ -170,20 +170,34 @@ using a different number of arguments each time.
 """
 def sandwich(ingredienti:list[str]=list[str]):
     print(f"gli ingredienti del panino sono {ingredienti}")
-sandwich("hamburger","pomodori","insalata")
+
+panino=sandwich(["hamburger","pomodori","insalata"])
+sandwich(["formaggio","hamburger"])
+sandwich(ingredienti=["hamburger"])
 
 """
 8-13. User Profile:  Build a profile of yourself by calling build_profile(), using your first and last names and three other 
 key-value pairs that describe you. All the values must be passed to the function as parameters. 
 The function then must return a string such as "Eric Crow, age 45, hair brown, weight 67"
 """
+def build_profile(firstname:str,lastname:str,age:int,hair:str,weight:int):
+    return f"{firstname} {lastname}, age {age}, hair {hair}, weight {weight}"
+profile=build_profile("lorenzo","trombini",19,"brown",75)
+print(profile)
 """
 8-14. Cars: Write a function that stores information about a car in a dictionary. The function should always 
 receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. 
-Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Your function should work for a call like this one: car = make_car('subaru', 'outback', color='blue', tow_package=True) Print the dictionary that’s returned to make sure all the information was stored correctly. 
+Call the function with the required information and two other name-value pairs, such as a color or an optional feature.
+ Your function should work for a call like this one: car = make_car('subaru', 'outback', color='blue', tow_package=True) 
+ Print the dictionary that’s returned to make sure all the information was stored correctly. 
 """
+def make_car(manufacturer:str,modelname:str,**kwargs):
+    
+
+    
 """
-8-15. Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py. Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.
+8-15. Printing Models: Put the functions for the example printing_models.py in a separate file called printing_functions.py. 
+Write an import statement at the top of printing_models.py, and modify the file to use the imported functions.
 """
 """
 8-16. Imports: Using a program you wrote that has one function in it, store that function in a separate file. Import the function into your main program file, and call the function using each of these approaches:
