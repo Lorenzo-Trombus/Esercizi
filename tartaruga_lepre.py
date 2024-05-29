e970c6e81d9d5ba427ce4879d1e17835aabbfba0
Nio4124 <antoniozottola41204@gmail.com>
2024-05-29 10:53:26 +0200

    esercizio senza sfide aggiuntive

--- tartaruga_lepre.py
+++ tartaruga_lepre.py
@@ -30,7 +30,11 @@ i contendenti si troveranno sullo stesso quadrato. In questo caso la tartaruga m
 deve stampare 'OUCH!!!' iniziando da quella posizione. Tutte le posizioni di stampa diverse dalla 'T', dalla 'H' o dal 'OUCH!!!' 
 (in caso della stessa posizione) devono essere il carattere '_'.
 
-Dopo la stampa di ogni tick, verificate se gli animali hanno raggiunto o superato il quadrato 70. Se è così, stampate il nome del vincitore e terminate la simulazione. Se vince la tartaruga, stampate "TORTOISE WINS! || VAY!!!". Se vince la lepre, stampate "HARE WINS || YUCH!!!". Se allo stesso tick dell'orologio vincono tutti e due gli animali, potreste voler favorire la tartaruga (la "sfavorita"), oppure stampare "IT'S A TIE.". Se non vince nessun animale, eseguite una nuova iterazione per simulare il successivo tick dell'orologio.
+Dopo la stampa di ogni tick, verificate se gli animali hanno raggiunto o superato il quadrato 70. Se è così, 
+stampate il nome del vincitore e terminate la simulazione. Se vince la tartaruga, stampate "TORTOISE WINS! || VAY!!!". 
+Se vince la lepre, stampate "HARE WINS || YUCH!!!". Se allo stesso tick dell'orologio vincono tutti e due gli animali, 
+potreste voler favorire la tartaruga (la "sfavorita"), oppure stampare "IT'S A TIE.". Se non vince nessun animale, 
+eseguite una nuova iterazione per simulare il successivo tick dell'orologio.
 
 Requisiti del Codice:
 - Utilizzare il modulo random per la generazione dei numeri casuali.
@@ -38,23 +42,28 @@ Requisiti del Codice:
     - una funzione per visualizzare le posizioni sulla corsia di gara,
     - una funzione per calcolare la mossa della tartaruga,
     - una funzione per calcolare la mossa della lepre.
-- Implementare un loop per simulare i tick dell'orologio. Ad ogni tick, calcolare le mosse, mostrare la posizione sulla corsia di gara, e determinare l'eventuale fine della gara.
+- Implementare un loop per simulare i tick dell'orologio. Ad ogni tick, calcolare le mosse, 
+mostrare la posizione sulla corsia di gara, e determinare l'eventuale fine della gara.
  
 SFIDE AGGIUNTIVE:
 1. Variabilità Ambientale:
 Introdurre fattori ambientali che possono influenzare la corsa, come il meteo.
-Ad esempio, la pioggia può ridurre la velocità di avanzamento o aumentare la probabilità di scivolate per entrambi i concorrenti. Implementare un sistema dove le condizioni 'soleggiato' e 'pioggia' cambiano dinamicamente ogni 10 tick dell'orologio.
+Ad esempio, la pioggia può ridurre la velocità di avanzamento o aumentare la probabilità di scivolate per entrambi i concorrenti. 
+ùImplementare un sistema dove le condizioni 'soleggiato' e 'pioggia' cambiano dinamicamente ogni 10 tick dell'orologio.
  
 Modificatori mossa:
 - La Tartaruga in caso di pioggia subisce penalità -1 su ogni mossa. In caso di sole non subisce variazioni.
 - La Lepre in caso di pioggia subisca una penalità -2 su ogni mossa. In caso di sole non subisce variazioni.
  
 2. Energia o Stamina:
-Aggiungere una metrica di "energia" o "stamina" che diminuisce con ogni movimento e si ricarica in specifiche condizioni. Fare in modo che le mosse che consumano più energia non possano essere eseguite se l'animale non ha abbastanza energia. L'energia inziale per entrambi gli animali è 100.
+Aggiungere una metrica di "energia" o "stamina" che diminuisce con ogni movimento e si ricarica in specifiche condizioni. 
+Fare in modo che le mosse che consumano più energia non possano essere eseguite se l'animale non ha abbastanza energia. 
+L'energia inziale per entrambi gli animali è 100.
 
 Nuove regole di movimento:
 - Tartaruga:
-    - Per la tartaruga, ogni volta che il numero generato indica una mossa ma non è possibile eseguirla per mancanza di energia, essa guadagna 10 di energia. Non può superare l'energia iniziale.
+    - Per la tartaruga, ogni volta che il numero generato indica una mossa ma non è possibile eseguirla per mancanza di energia, 
+    essa guadagna 10 di energia. Non può superare l'energia iniziale.
     - Passo veloce (50% di probabilità): avanza di 3 quadrati e richiede 5 di energia.
     - Scivolata (20% di probabilità): arretra di 6 quadrati e richiede 10 di energia. Non può andare sotto il quadrato 1.
     - Passo lento (30% di probabilità): avanza di 1 quadrato e richiede 3 di energia.
@@ -67,52 +76,108 @@ Nuove regole di movimento:
     - Piccola scivolata (20% di probabilità): arretra di 2 quadrati e richiede 8 di energia. Non può andare sotto il quadrato 1.
  
 3. Ostacoli e Bonus
-Sulla pista di gara sono presenti alcuni ostacoli e bonus a posizioni fisse, che influenzano direttamente il movimento degli animali quando vengono calpestati. Gli ostacoli causano uno slittamento all'indietro, mentre i bonus offrono un avanzamento extra.
+Sulla pista di gara sono presenti alcuni ostacoli e bonus a posizioni fisse, che influenzano direttamente il movimento degli 
+animali quando vengono calpestati. Gli ostacoli causano uno slittamento all'indietro, mentre i bonus offrono un avanzamento extra.
 
 Dettagli Implementativi:
 - Ostacoli:
-Posizionati a intervalli regolari sulla pista (es. ai quadrati 15, 30, 45), gli ostacoli riducono la posizione dell'animale di un numero specificato di quadrati (es: -3, -5, -7). Gli ostacoli sono rappresentati da un dizionario che mappa le posizioni degli ostacoli sul percorso (chiave) ed i relativi effetti (valore). Assicurarsi che nessun animale retroceda al di sotto del primo quadrato a seguito di un ostacolo.
+Posizionati a intervalli regolari sulla pista (es. ai quadrati 15, 30, 45), gli ostacoli riducono la posizione dell'animale 
+di un numero specificato di quadrati (es: -3, -5, -7). 
+Gli ostacoli sono rappresentati da un dizionario che mappa le posizioni degli ostacoli sul percorso (chiave) ed i relativi effetti 
+(valore). Assicurarsi che nessun animale retroceda al di sotto del primo quadrato a seguito di un ostacolo.
 
 - Bonus:
-Dislocati strategicamente lungo la corsa (es. ai quadrati 10, 25, 50), i bonus aumentano la posizione dell'animale di un numero determinato di quadrati (es: 5, 3, 10). I bonus sono rappresentati da un dizionario che mappa le posizioni dei bonus sul percorso (chiave) ed i relativi effetti (valore). Consentire agli animali di beneficiare pienamente dei bonus, ma non oltrepassare il traguardo.
+Dislocati strategicamente lungo la corsa (es. ai quadrati 10, 25, 50), i bonus aumentano la posizione dell'animale 
+di un numero determinato di quadrati (es: 5, 3, 10). I bonus sono rappresentati da un dizionario che mappa le posizioni 
+dei bonus sul percorso (chiave) ed i relativi effetti (valore). Consentire agli animali di beneficiare pienamente dei bonus, 
+ma non oltrepassare il traguardo.
 """
 import random
 
 numero_casuale=random.randint(1,10)
 
+posizione_lepre=1
+posizione_tartaruga=1
+
 tempo=0
-percorso=list(range(1,71))
-indice_lepre=13
-indice_tartaruga=15
-posizione_lepre=percorso[indice_lepre]
-posizione_tartaruga=percorso[indice_tartaruga]
+
+
 print("BANG!!!!!! AND THEY'RE OFF!!!!")
-percorso_grafico=list(range(1,71))
+
 
 def posizioni_sul_percorso():
-    
-    for i in percorso_grafico:
+    percorso_grafico=[]
+    for i in range(1,71):
         if i==posizione_lepre and posizione_lepre==posizione_tartaruga:
-            i="OUCH!!!"
-        elif percorso_grafico.index(i)==posizione_tartaruga:
+            i="T OUCH!!! H"
+            percorso_grafico.append(i)
+        elif i==posizione_tartaruga:
             i="T"
-        elif percorso_grafico.index(i)==posizione_lepre:
+            percorso_grafico.append(i)
+        elif i==posizione_lepre:
             i="H"
+            percorso_grafico.append(i)
         else:
             i="_"
+            percorso_grafico.append(i)
 
     return percorso_grafico
 
 
+    
+
+
 def mossa_tartaruga():
-    pass 
+    global posizione_tartaruga
+    num_casual_tartaruga=random.randint(1,10)
+    if num_casual_tartaruga <= 5:
+        posizione_tartaruga+=3
+    elif num_casual_tartaruga>5 and num_casual_tartaruga<8:
+        if posizione_tartaruga>6:
+            posizione_tartaruga-=6
+        else:
+            posizione_tartaruga=1
+    elif num_casual_tartaruga>=8:
+        posizione_tartaruga+=1
+
 
 def mossa_lepre():
-    pass
+    global posizione_lepre
+    num_casual_lepre=random.randint(1,10)
+    if num_casual_lepre>2 and num_casual_lepre<5:
+        posizione_lepre+=9
+    elif num_casual_lepre==5:
+        if posizione_lepre>12:
+            posizione_lepre-=12
+        else:
+            posizione_lepre=1
+    elif num_casual_lepre>5 and num_casual_lepre<9:
+        posizione_lepre+=1
+    elif num_casual_lepre>8:
+        if posizione_lepre>1:
+            posizione_lepre-=1
+        else:
+            posizione_lepre=1
+    
 
-#while posizione_lepre<70 or posizione_tartaruga < 70:
- #   pass
+while posizione_lepre<70 or posizione_tartaruga < 70:
+    tempo+=1
+    print(f"Tempo: {tempo} sec")
+    mossa_tartaruga()
+    mossa_lepre()
+    print(posizioni_sul_percorso())
+    if posizione_lepre>=70 and posizione_tartaruga>=70:
+        print("ITS A TIE")
+        break
+    elif posizione_tartaruga>=70:
+        print("TORTOISE WINS||YUCH!!!!")
+        break
+    elif posizione_lepre>=70:
+        print("HARE WINS|| VAY!!!!")
+        break
+    
 
-print(posizioni_sul_percorso())
+
+
 
 
