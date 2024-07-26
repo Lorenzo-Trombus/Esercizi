38babe6b65d40c01711a9096e9c82fbd42f511e8
Lorenzo-Trombus <lorenzotrombini28@gmail.com>
2024-07-26 09:11:53 +0200

    sto per fare l'esame

--- riepilogo_ripasso.py
+++ riepilogo_ripasso.py
@@ -252,7 +252,7 @@ class Customer:
         
 
 class VideoRentalStore:
-    def __init__(self, movies:dict[str,Movie]=dict(),customers:dict[str,Customer]=dict()) -> None:
+    def __init__(self, movies:dict[str,Movie]=dict[str,Movie],customers:dict[str,Customer]=dict[str,Customer]) -> None:
         self.movies=movies
         self.customers=customers
     
@@ -367,18 +367,35 @@ Metodi:
 
 
 
-"""Sviluppa un sistema per la gestione delle ricette in Python che permetta agli utenti di creare, modificare, e cercare ricette basate sugli ingredienti. Il sistema dovrà essere capace di gestire una collezione (dizionario) di ricette e i loro ingredienti.
+"""Sviluppa un sistema per la gestione delle ricette in Python che permetta agli utenti di creare, modificare, 
+e cercare ricette basate sugli ingredienti. Il sistema dovrà essere capace di gestire una collezione (dizionario) 
+di ricette e i loro ingredienti.
 Classe:
 - RecipeManager:
     Gestisce tutte le operazioni legate alle ricette.
     Metodi:
-    - create_recipe(name, ingredients): Crea una nuova ricetta con il nome specificato e una lista di ingredienti. Restituisce un nuovo dizionario con la sola ricetta appena creata o un messaggio di errore se la ricetta esiste già.
-    - add_ingredient(recipe_name, ingredient): Aggiunge un ingrediente alla ricetta specificata. Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente esiste già o la ricetta non esiste.
-    - remove_ingredient(recipe_name, ingredient): Rimuove un ingrediente dalla ricetta specificata. Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente non è presente o la ricetta non esiste.
-    - update_ingredient(recipe_name, old_ingredient, new_ingredient): Sostituisce un ingrediente con un altro nella ricetta specificata. Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente non è presente o la ricetta non esiste.
+    - create_recipe(name, ingredients): Crea una nuova ricetta con il nome specificato e una lista di ingredienti. 
+    Restituisce un nuovo dizionario con la sola ricetta appena creata o un messaggio di errore se la ricetta esiste già.
+    - add_ingredient(recipe_name, ingredient): Aggiunge un ingrediente alla ricetta specificata. 
+    Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente esiste già o la ricetta non esiste.
+    - remove_ingredient(recipe_name, ingredient): Rimuove un ingrediente dalla ricetta specificata. 
+    Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente non è presente o la ricetta non esiste.
+    - update_ingredient(recipe_name, old_ingredient, new_ingredient): Sostituisce un ingrediente con un altro nella ricetta specificata. 
+    Restituisce la ricetta aggiornata o un messaggio di errore se l'ingrediente non è presente o la ricetta non esiste.
     - list_recipes(): Elenca tutte le ricette esistenti.
-    - list_ingredients(recipe_name): Mostra gli ingredienti di una specifica ricetta. Restituisce un elenco di ingredienti o un messaggio di errore se la ricetta non esiste.
-    - search_recipe_by_ingredient(ingredient): Trova e restituisce tutte le ricette che contengono un determinato ingrediente. Restituisce un elenco di ricette o un messaggio di errore se nessuna ricetta contiene l'ingrediente."""
+    - list_ingredients(recipe_name): Mostra gli ingredienti di una specifica ricetta. 
+    Restituisce un elenco di ingredienti o un messaggio di errore se la ricetta non esiste.
+    - search_recipe_by_ingredient(ingredient): Trova e restituisce tutte le ricette che contengono un determinato ingrediente. 
+    Restituisce un elenco di ricette o un messaggio di errore se nessuna ricetta contiene l'ingrediente."""
+ 
+
+class RecipeManager:
+    def __init__(self) -> None:
+        self.list_ricette=[]
+    
+    def create_recipe(self,name:str,ingredients:list[str]):
+        if name not in self.list_ricette:
+
 
 
 ############################
